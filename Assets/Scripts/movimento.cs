03b1e1f6e3a4e895d07813087752af4d378785bf
sebastgit <playandroidhb@gmail.com>
2019-10-16 16:01:04 -0300

    movimento

---
+++ Assets/Scripts/movimento.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+{
+    public int velo = 5;
+    private float controle;
+    void Update()
+    {
+        controle = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(controle * velo * Time.deltaTime, 0));
+    }
+}
