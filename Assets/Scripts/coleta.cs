3edaf0839974fc9ab477647650f3088a58c9ead4
sebastgit <playandroidhb@gmail.com>
2019-10-16 16:44:24 -0300

    adcao coleta de moedas

---
+++ Assets/Scripts/coleta.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coleta : MonoBehaviour
+{
+    private int pontos = 0;
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D outro)
+    {
+        if (outro.gameObject.CompareTag("moeda"))
+        {
+           
+        }
+    }
+}
