754abdae325880cc82cb48695fa2973a0226a237
sebastgit <playandroidhb@gmail.com>
2019-10-16 16:17:42 -0300

    adicionado pulo

--- Assets/Scripts/movimento.cs
+++ Assets/Scripts/movimento.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class movimento : MonoBehaviour
 {
     public int velo = 5;
-    private float controle;
+    private float controle, forca = 250f;
+    public int duplo = 2;
+    public Rigidbody2D puto;
     void Update()
     {
         controle = Input.GetAxis("Horizontal");
         transform.Translate(new Vector2(controle * velo * Time.deltaTime, 0));
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+            pulo();
+            }
+    }
+    public void pulo() {
+        if (duplo > 0)
+        {
+            
+            puto.AddForce(new Vector2(0, forca * Time.deltaTime), ForceMode2D.Impulse);
+            duplo--;
+            
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D outro)
+    {
+        if (outro.gameObject.CompareTag("chao"))
+        {
+            duplo = 2;
+        }
     }
 }
