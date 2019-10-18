f3d1446b17174b8ca3b5003c65c9266e6e0c6ee2
sebastgit <playandroidhb@gmail.com>
2019-10-18 16:16:05 -0300

    alguma coisa ai

--- Assets/Scripts/coleta.cs
+++ Assets/Scripts/coleta.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class coleta : MonoBehaviour
 {
     public int pontos = 0;
-    
+    public GameObject g;
+    private Text omg;
+    private AudioSource audio;
+    private void Start()
+    {
+        omg = g.GetComponent<Text>();
+        audio = GetComponent<AudioSource>();
+    }
     void Update()
     {
-        
+        omg.text = "pontos: " + pontos;
     }
     private void OnCollisionEnter2D(Collision2D outro)
     {        
@@ -16,6 +24,7 @@ public class coleta : MonoBehaviour
         {
             Destroy(outro.gameObject);
             pontos++;
+            audio.PlayOneShot(audio.clip);
         }
     }
 }
